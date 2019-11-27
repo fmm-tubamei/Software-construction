ef7eaf89e32a66142589a4ff09ef75ac79361190
方二梅 <@LAPTOP-OU5PMUBE>
2019-11-27 23:14:14 +0800

    send

---
+++ src/fmm/AdditionOperation.java
@@ -0,0 +1,13 @@
+package fmm;
+
+public class AdditionOperation extends BinaryOperator {
+	public AdditionOperation() {
+		generateBinaryOperator('+');
+	}
+	int calculate(int left, int right) {
+		return left + right;
+	}
+	boolean check(int results) {
+		return results <= UPPER;
+	}
+}
